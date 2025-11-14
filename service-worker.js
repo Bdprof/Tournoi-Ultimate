1b4f5e02676aa11519cbf3138c574728c76a6631
Bdprof <benoitdarlon@gmail.com>
2025-11-14 17:54:53 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,23 @@
 
-const CACHE_NAME = "ultimate-v1";
+const CACHE_NAME = "ultimate-v2";
 const ASSETS = [
   "index.html",
   "manifest.webmanifest",
+  "service-worker.js",
   "icon-192.png",
   "icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
